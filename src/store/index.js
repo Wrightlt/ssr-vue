b674888eb279a7d25e10257a1b3dd5e433e6c639
laite <laite@shoplex.com>
2018-04-08 14:00:54 +0800

    add vuex

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as actions from './actions'
+import * as mutations from './mutations'
+
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+export default new Vuex.Store({
+  actions,
+  mutations,
+  modules: {
+  },
+  strict: debug
+})
